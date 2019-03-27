7c8a258d9e1cccf01291f73a546ff089a71521b7
map0 <vortep@protonmail.com>
2019-03-27 23:55:02 +0200

    + config file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const config = require('./config/config');
 
 const app = express();
 
@@ -8,6 +9,6 @@ app.get('/', (req, res, next) => {
 
 app.set('port', process.env.PORT || 7777)
 
-app.listen(app.get('port'), () => {
-  console.log(`Express is running. Please open http://localhost:${app.get('port')}`)
+app.listen(config.port, () => {
+  console.log(`Express is running. Please open http://localhost:${config.port}`)
 })
