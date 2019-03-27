b0e2e2b41dacb915ae4a5495f8a7e0b0e3e1a4dc
map0 <vortep@protonmail.com>
2019-03-28 01:26:56 +0200

    quick-n-dirty save to mongodb

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
 const express = require('express');
-const logger = require('pino')()
+const bodyParser = require('body-parser');
+const logger = require('pino')();
 const config = require('./config/config');
 
 const app = express();
+
+require('dotenv').config({ path: 'variables.env' });
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+/*
+ * quickly setup db-connection
+ */
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise
+
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true })
+
+mongoose.connection.on('error', (err) => {
+  logger.error(`ouch -> ${err.message}`)
+})
+
+mongoose.connection.on('error', () => {
+  logger.error('connection error with mongodb');
+});
+
+mongoose.connection.on('open', () => {
+  logger.info('connected to mongodb on');
+});
+
+mongoose.connection.on('disconnected', () => {
+  logger.error('Mongoose default connection disconnected');
+});
+
+/**
+ * define a basic mongoDB Model for user
+ */
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+const UserModel = mongoose.model('User', userSchema);
 
 app.get('/', (req, res, next) => {
   res.send('hello world');
 })
 
-app.set('port', process.env.PORT || 7777)
+app.post('/', (req, res, next) => {
+  const newUser = new UserModel({ username: req.body.username });
+  newUser.save();
+})
 
 app.listen(config.port, () => {
   logger.info(`Express is running. Please open http://localhost:${config.port}`)
