83f585ef876d788426b453f8b58a38d31e6ce3de
map0 <vortep@protonmail.com>
2019-03-28 11:40:15 +0200

    loader for models and controllers

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ require('app-module-path').addPath(__dirname);
 const log = require('lib/log')
 const mongoose = require('lib/mongoose')
 const express = require('lib/express');
+const loader = require('lib/loader');
 const exceptions = require('exceptions');
 const routes = require('routes');
 const config = require('config/config');
-const UserModel = require('models/user');
+// const UserModel = require('models/user');
 const packageJson = require('package');
 
 require('dotenv').config({ path: 'variables.env' });
@@ -22,14 +23,17 @@ app.config = config
 
 app.log = log(app);
 
+app.mongoose = mongoose(app)
+
+// app.models = {
+//   user: UserModel(app)
+// }
+app.models = loader(app, 'models');
+
 app.express = express(app);
+
+app.controllers = loader(app, 'controllers');
 
 app.routes = routes(app);
 
 app.exceptions = exceptions(app);
-
-app.mongoose = mongoose(app)
-
-app.models = {
-  user: UserModel(app)
-}
