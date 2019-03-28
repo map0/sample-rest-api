3ee9a18ccbe90c4470e3b706478f61321641d21e
map0 <vortep@protonmail.com>
2019-03-28 09:25:08 +0200

    factor out express

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require('app-module-path').addPath(__dirname);
 
-const express = require('express')();
-const bodyParser = require('body-parser');
 const log = require('lib/log')
 const mongoose = require('lib/mongoose')
+const express = require('lib/express');
 const config = require('config/config');
 const UserModel = require('models/user');
 const packageJson = require('package')
+
+require('dotenv').config({ path: 'variables.env' });
 
 const app = {
   name: packageJson.name,
@@ -19,28 +20,10 @@ app.config = config
 
 app.log = log(app);
 
-require('dotenv').config({ path: 'variables.env' });
-
-express.use(bodyParser.json())
-express.use(bodyParser.urlencoded({ extended: true }))
+app.express = express(app);
 
 app.mongoose = mongoose(app)
 
 app.models = {
   user: UserModel(app)
 }
-
-express.get('/', (req, res, next) => {
-  res.send('hello world');
-})
-
-express.post('/', (req, res, next) => {
-  const User = app.models.user
-  const newUser = new User({ username: req.body.username });
-  newUser.save();
-  res.send('data created')
-})
-
-express.listen(app.config.port, () => {
-  app.log.info(`Express is running. Please open http://localhost:${app.config.port}`)
-})
