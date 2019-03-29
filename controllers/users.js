b163fb792f8ac63c6e60ce0c7cd0d713b84c53f8
map0 <vortep@protonmail.com>
2019-03-29 11:46:45 +0200

    user.index to return  available routes

--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,45 @@
 module.exports = app => {
   const User = app.models.user;
 
+  function fetchRoutes() {
+    const routes = []
+
+    app.routes.stack.forEach(route => routes.push({
+      path: route.route.path,
+      methods: route.route.methods
+    }))
+    return routes
+  }
+
   return {
     async index(req, res) {
-      res.send('hello world!!!');
+      const availableRoutes = fetchRoutes()
+      res.status(200);
+      res.json({
+        message: 'Welcome to this awesome-soon-to-be restapi',
+        routes: availableRoutes
+      })
     },
     async add(req, res) {
       const newUser = new User({ username: req.body.username });
       try {
         await newUser.save();
+        res.status(201);
         return res.json({
           status: 'data created',
           token: res.token
         });
       } catch (e) {
-        if (e.errmsg.match('duplicate key')) {
-          return res.send('ooops. this record exists')
+        if (e.errmsg && e.errmsg.match('duplicate key')) {
+          res.status(403);
+          return res.json({
+            message: 'ooops. this record exists',
+          })
         }
-        return res.send('smt went wrong while storing the record')
+        res.status(500);
+        return res.json({
+          message: 'smt went terribly wrong while storing the record'
+        })
       }
     }
   }
